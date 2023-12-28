570560f9cb0da4ba0c90f8bbdbe83932a2e0c783
KaryNunes <150854635+KaryNunes@users.noreply.github.com>
2023-12-28 15:56:30 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+const lista = ["Autoavaliação:\n Identificar minhas habilidades, interesses e valores.\n", "Definir Metas:\n Estabelecer metas de curto, médio e longo prazo.\n", "Desenvolvimento de Habilidades:\n Identificar as habilidades necessárias para atingir suas metas.\n", "Networking:\n Construir e manter uma rede profissional.\n", "Exploração de Oportunidades:\n Pesquisar oportunidades de carreira na minha área.\n", "Educação Continuada:\n Considerar programas de graduação ou certificações relevantes.\n", "Experiência Prática:\n Buscar estágios, projetos voluntários ou trabalhos temporários.\n"]
+
+console.log ("Plano de Carreira:");
+for (const item of lista){
+console.log(item)
+}
